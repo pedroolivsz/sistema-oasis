06cb928ad148645d8b4cc4ca8586b3411eed5788
João Pedro <joaopedropereira2209@gmail.com>
2026-01-29 17:54:14 -0300

    feat(serviceexception): new override method receiving throwable

--- src/main/java/com/io/github/pedroolivsz/service/ServiceException.java
+++ src/main/java/com/io/github/pedroolivsz/service/ServiceException.java
@@ -4,4 +4,7 @@ public class ServiceException extends RuntimeException {
     public ServiceException(String message) {
         super(message);
     }
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
